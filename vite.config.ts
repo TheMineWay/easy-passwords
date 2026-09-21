36a040893391df8d17a33e7c83c95d0fd885ffd6
TheMineWay <joelcamposoliva9000@gmail.com>
2026-09-21 18:39:41 +0200

    feat: :wrench: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -22,8 +22,14 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: '/logo.png',
-            sizes: '192x192 224x224 512x512',
+            src: '/pwa-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: '/pwa-512.png',
+            sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
           }
